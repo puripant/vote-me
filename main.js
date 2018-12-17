a7ca7442182deacf3f2fdd731786ad9e2f4b235b
Puripant <puripant@gmail.com>
2018-12-17 15:12:39 +0700

    First commit: basic label swap

---
+++ main.js
@@ -0,0 +1,15 @@
+const ballot = document.getElementById('ballot');
+const me = document.getElementById('me');
+
+function swap_me(here) {
+  if (here.id !== me.id) {
+    //loop to find #me
+    for (let el = here.previousSibling; el != null; el = el.previousSibling) {
+      if (el.id === me.id) { //#me found before here
+        ballot.insertBefore(me, here.nextSibling);
+        return;
+      }
+    }
+    ballot.insertBefore(me, here);
+  }
+}
